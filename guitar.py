0b85b2db2a0f41c2c48e67c83c19d386335d143d
Jahid <jahidul.fci@gmail.com>
2025-07-23 23:13:34 +1000

    All changes made

--- guitar.py
+++ guitar.py
@@ -16,6 +16,7 @@ class Guitar:
         """Return True if the guitar is 50 or more years old."""
         return self.get_age() >= 50
 
+
     def __str__(self):
         """Return a string representation of the guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
